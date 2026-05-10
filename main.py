d672f5a4c6d3874e0bc5dd3b412ffcd2a704eb32
kaede221 <kaedeshimizu@qq.com>
2026-05-10 13:36:55 +0800

    init: 实现了简单的开屏字符画

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from cleansh!")
+from rich.console import Console
+from rich.panel import Panel
 
+console = Console()
 
-if __name__ == "__main__":
-    main()
+console.print(
+    Panel(
+        """   ____   _       _____      _      _   _   ____    _   _ 
+  / ___| | |     | ____|    / \\    | \\ | | / ___|  | | | |
+ | |     | |     |  _|     / _ \\   |  \\| | \\___ \\  | |_| |
+ | |___  | |___  | |___   / ___ \\  | |\\  |  ___) | |  _  |
+  \\____| |_____| |_____| /_/   \\_\\ |_| \\_| |____/  |_| |_|
+""",
+        title="Kaede Shimizu",
+        subtitle="0.0.1",
+    )
+)
